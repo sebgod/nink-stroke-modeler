1cab6bc69f03e47703089a00bb797c19afc9953e
Sebastian Godelet <sebastian.godelet@outlook.com>
2024-07-22 19:35:29 +1000

    CI/CD: Windows

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,4 @@ exec docker run -e USER="$(id -u)" -u="$(id -u):$(id -g)" \
    -v /tmp/build_output:/tmp/build_output \
    -w /src/$PWD -it gcr.io/bazel-public/bazel:latest \
    --output_user_root=/tmp/build_output \
-   build //:all
+   build --config=gcc //:all
